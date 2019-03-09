b8de3c9992711ac3aa4568faaa431ffff0b7661f
Eyder ACM <eyderacm@gmail.com>
2019-03-09 12:51:21 -0600

    add documentation

--- EjerciciosExtra.c
+++ EjerciciosExtra.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-
-#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
-#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
 /*Hacer un juego de gato, donde la máquina cálcula todas las posibles soluciones, y coloca un movimiento
 después de determinar la mejor posibilidad de ganar, se usará árbol, y la computadorá tendrá una estrategia para ganar*/
 
@@ -34,6 +30,12 @@ Procedimiento general:
 -Se repite hasta el fin del juego
 */
 
+#include <stdio.h>
+
+// Definimos para utilizar en algoritmo Minimax
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
+
 void imprimirTabla(char[][3]);
 void movimientoJugador(char[][3]);
 int movimientoIdeal(char[][3], int, int);
@@ -63,11 +65,11 @@ int main(){
 
     // Verificamos de forma termino el juego, victoria de maquina, jugador, o empate
     if(esEmpate(tabla)){
-        printf("Hemos empatado!");
+        printf("\nHemos empatado!");
     }else if(determinaPuntuacion(tabla) == 10){
-        printf("He ganado!");
+        printf("\nHe ganado! Perdedor");
     }else{
-        printf("Felicidades, has ganado!");
+        printf("\nFelicidades, has ganado!");
     }
 }
 
@@ -120,56 +122,75 @@ void colocarMovimiento(char tabla[][3], int columna, int fila, char movimiento){
 }
 
 void turnoMaquina(char tabla[][3]){
+    // Determinamos una puntuacion, esta sera reemplazada por los movimientos probados por la maquina
     int maximaPuntuacion = -100;
     int filaIdeal;
     int columnaIdeal;
     for (int filas = 0; filas < 3; filas++){
         for (int columnas = 0; columnas < 3; columnas++){
             if(tabla[columnas][filas] == '_'){
+                // Buscamos los espacios vacios, los reemplazamos con un movimiento de la maquina
                 tabla[columnas][filas] = 'o';
+                // Comprobamos si la probabilidad de ganar de ese movimiento es mayor a la maxima probabilidad, de serlo, almacenamos la fila y columna
                 if(movimientoIdeal(tabla, 0, 0) > maximaPuntuacion){
                     maximaPuntuacion = movimientoIdeal(tabla, 0, 0);
                     filaIdeal = filas;
                     columnaIdeal = columnas;
                 }
+                // Volvemos a vaciar el espacio, para probar otro movimiento
                 tabla[columnas][filas] = '_';
             }
         }
     }
+    // Se coloca el movimiento en el espacio con la mayor probabilidad de ganar
     colocarMovimiento(tabla, columnaIdeal, filaIdeal, 'o');
 }
 
 int movimientoIdeal(char tabla[][3], int esTurnoMaquina, int profundidad){
+    // Determinamos si el juego ha terminado, si gana la maquina, determinamos 10 puntos, si gana el jugador, -10
     int puntuacion = determinaPuntuacion(tabla);
+
+    // De haber ganado la maquina, retornamos la puntuacion, menos la profunidad, esto para ganar en la menor cantidad de pasos posibles
     if(puntuacion == 10){
         return (puntuacion-profundidad);
     }
 
+    // De haber el jugador, retornamos la puntuacion mas la profundidad, esto por si la maquina ha de perder, hacerlo en la mayor cantidad de pasos posibles
     if(puntuacion == -10){
         return(puntuacion+profundidad);
     }
 
+    //De ser empate retornamos 0
     if(esEmpate(tabla)){
         return 0;
     }
+
+    // Si el turno a simular es de la maquina
     if(esTurnoMaquina){
+        // Definimos el mejor resultado posible como -100, ya que la maquina al ganar, debera retornar un resultado positivo
         int mejor = -100;
         for(int filas = 0; filas < 3; filas++){
             for(int columnas = 0; columnas < 3; columnas++){
                 if(tabla[columnas][filas] == '_'){
+                    // Probamos un movimiento de la maquina
                     tabla[columnas][filas] = 'o';
+                    // Determinamos la mayor probabilidad entre el movimiento encontrado, y el actual
                     mejor = MAX(movimientoIdeal(tabla, !esTurnoMaquina, profundidad+1), mejor);
                     tabla[columnas][filas] = '_';
                 }
             }
         }
         return mejor;
+    // Si el turno a simular es del jugador
     }else {
+        // Definimos el mejor resultado posible como 100, ya que la maquina al perder, debera retornar un resultado negativo
         int mejor = 100;
         for(int filas = 0; filas < 3; filas++){
             for(int columnas = 0; columnas < 3; columnas++){
                 if(tabla[columnas][filas] == '_'){
+                    // Simulamos un movimiento del jugador
                     tabla[columnas][filas] = 'x';
+                    // Determinamos cual es la menor probabilidad, el movimiento actual, o el siguiente
                     mejor = MIN(movimientoIdeal(tabla, !esTurnoMaquina, profundidad+1), mejor);
                     tabla[columnas][filas] = '_';
                 }
@@ -179,10 +200,13 @@ int movimientoIdeal(char tabla[][3], int esTurnoMaquina, int profundidad){
     }
 }
 
+
 int esEmpate(char tabla[][3]){
     int empataron = 1;
+    // Determinamos si la tabla y sus movimientos resultan un empate
     for(int filas = 0; filas < 3; filas++){
         for(int columnas = 0; columnas < 3; columnas++){
+            // De no encontrar ningun espacio vacio se retornara un empate (cabe resaltar que esta funcion se utilizaza con otra comparacion de ganador)
             if(tabla[columnas][filas] == '_'){
                 empataron = 0;
             }
@@ -196,6 +220,7 @@ int determinaPuntuacion(char tabla[][3]) {
     // Comprobamos de izquierda a derecha
     for(int fila = 0; fila < 3; fila++){
         if((tabla[0][fila] == tabla[1][fila]) && (tabla[1][fila] ==  tabla[2][fila])){
+            // Verificamos si el ganador es el jugador o la maquina
             if(tabla[0][fila] == 'o'){
                 puntuacion = 10;
             }else if(tabla[0][fila] == 'x'){
@@ -207,6 +232,7 @@ int determinaPuntuacion(char tabla[][3]) {
     // Comprobamos de arriba abajo
     for(int columna = 0; columna < 3; columna++){
         if(tabla[columna][0] == tabla[columna][1] && tabla[columna][1] == tabla[columna][2]){
+            // Verificamos si el ganador es el jugador o la maquina
             if(tabla[columna][0] == 'o'){
                 puntuacion = 10;
             }else if(tabla[columna][0] == 'x'){
@@ -217,12 +243,14 @@ int determinaPuntuacion(char tabla[][3]) {
 
     // Comprobamos diagonales
     if((tabla[0][0] == tabla[1][1] && tabla[1][1] == tabla[2][2]) || (tabla[0][2] == tabla [1][1] && tabla[1][1] == tabla[2][0])){
+        // Verificamos si el ganador es el jugador o la maquina
         if(tabla[1][1] == 'o'){
             puntuacion = 10;
         }else if(tabla[1][1] == 'x'){
             puntuacion = -10;
         }
     }
+    // Retornamos una puntuacion representando quien gano
     return puntuacion;
 }
 
