af32064f5a578e7e2977cf1e27da548dd3a6910f
Eyder ACM <eyderacm@gmail.com>
2019-03-08 21:44:58 -0600

    starting algorithm

---
+++ EjerciciosExtra.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//Ejercicio 1
+/*Hacer Gauss jordan de matriz
+Obtener matriz identidad
+Entradas Random enteras*/
+
+//Ejercicio 2
+/*Hacer comprobador de sudoku, comprobar que matriz si matriz de 3 por 3 es sudoku, usando random para cada bloque*/
+
+//Ejercicio 3
+/*Hacer un juego de gato, donde la máquina cálcula todas las posibles soluciones, y coloca un movimiento
+después de determinar la mejor posibilidad de ganar, se usará árbol, y la computadorá tendrá na estrategia para gana    r*/
+
+//Ejercicio 4
+/*Lee cadena que representa una operación aritmética, (3*(4+5)/(8+(3*15)))
+Acepta: sumas, restas, multiplicaciones y paréntesis
+Usa un vector para operadores y para operandos
+[4, 5] [+]
+Hace la operación y los saca de la fila, luego continúa
+Comprobar primer si es sintácticamente correcto
+*/
+
+int main(){
+
+}
+
+
+
