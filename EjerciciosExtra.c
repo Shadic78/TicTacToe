13f312f92d66d7006bdaf740bcb6422862296620
eyder <eyderacm@gmail.com>
2019-03-17 16:43:53 -0600

    add simulation and number of gamesPlayed showcase

--- EjerciciosExtra.c
+++ EjerciciosExtra.c
@@ -1,30 +1,30 @@
-/*Hacer un juego de gato, donde la máquina cálcula todas las posibles soluciones, y coloca un movimiento
-después de determinar la mejor posibilidad de ganar, se usará árbol, y la computadorá tendrá una estrategia para ganar*/
+/*Hacer un juego de gato, donde la mï¿½quina cï¿½lcula todas las posibles soluciones, y coloca un movimiento
+despuï¿½s de determinar la mejor posibilidad de ganar, se usarï¿½ ï¿½rbol, y la computadorï¿½ tendrï¿½ una estrategia para ganar*/
 
 /*
 Autor:Eyder Concha Moreno 09/Marzo/19
-Entradas: Una posición en columnas y filas del 1 al 3
+Entradas: Una posiciï¿½n en columnas y filas del 1 al 3
 Salida: una tabla con la jugada de la IA
 Procedimiento general:
 -Se pregunta al jugador donde desea hacer un movimiento
--Se verifica si el lugar elegido es válido (no está ocupado en la tabla)
+-Se verifica si el lugar elegido es vï¿½lido (no estï¿½ ocupado en la tabla)
 -Se coloca en la tabla
--La IA encuentra los espacios vacíos en la tabla y coloca un movimiento en cada uno de ellos
+-La IA encuentra los espacios vacï¿½os en la tabla y coloca un movimiento en cada uno de ellos
 -En cada movimiento realizado, se verifica cada uno de los siguientes posibles movimientos, para realizar esto se hace lo siguiente:
-  *Se verifica si la simulación a terminado, de hacerlo se revisa si el ganador es el jugador, o la máquina
-      -De ser la máquina, el árbol devuelve 10 puntos menos la profundidad, esto para garantizar que haga el movimiento más cercano a ganar
-      -De ser el jugador, el árbol devuelve 10 puntos máss la profundidad, esto por si es de perder, retrase su derrota lo máximo posible
+  *Se verifica si la simulaciï¿½n a terminado, de hacerlo se revisa si el ganador es el jugador, o la mï¿½quina
+      -De ser la mï¿½quina, el ï¿½rbol devuelve 10 puntos menos la profundidad, esto para garantizar que haga el movimiento mï¿½s cercano a ganar
+      -De ser el jugador, el ï¿½rbol devuelve 10 puntos mï¿½ss la profundidad, esto por si es de perder, retrase su derrota lo mï¿½ximo posible
       -De no haber terminado, retorna 0
   *Se determina si el juego ha terminado en empate, de terminar en empate se retorna un 0
-  *Se determina si el turno a simular es del jugador, o de la máquina, se realiza lo siguiente:
-      -De ser de la máquina, se toma como referencia un valor de -100, se buscan los espacios vacios, y se reemplazan con un movimiento de la máquina.
-        Este movimiento se pasa a la misma función, comparando el valor que esta función ha de retornar con la misma variable de la probabilidad mayor.
+  *Se determina si el turno a simular es del jugador, o de la mï¿½quina, se realiza lo siguiente:
+      -De ser de la mï¿½quina, se toma como referencia un valor de -100, se buscan los espacios vacios, y se reemplazan con un movimiento de la mï¿½quina.
+        Este movimiento se pasa a la misma funciï¿½n, comparando el valor que esta funciï¿½n ha de retornar con la misma variable de la probabilidad mayor.
 
-      -De ser del Jugador, se toma como referencia un valor de 100, se buscan los espacios vacios, y se reemplazan con un movimiento de la máquina.
-        Este movimiento se pasa a la misma función, comparando el valor que esta función ha de retornar con la misma variable de la probabilidad mayor.
-  *Se retorna el valor encontrado por cada funcion, de esta manera recursivamente se verifican todas las posibles jugadas a realizar por la máquina o el jugador
+      -De ser del Jugador, se toma como referencia un valor de 100, se buscan los espacios vacios, y se reemplazan con un movimiento de la mï¿½quina.
+        Este movimiento se pasa a la misma funciï¿½n, comparando el valor que esta funciï¿½n ha de retornar con la misma variable de la probabilidad mayor.
+  *Se retorna el valor encontrado por cada funcion, de esta manera recursivamente se verifican todas las posibles jugadas a realizar por la mï¿½quina o el jugador
 -El algoritmo retorna un valor, este valor representando las probabilidades de ganar en cada movimiento, siendo estas negativas (si ha de perder), o positivas (si ha de ganar)
--Se determina que espacio vacío cuenta con la mayor probabilidad de ganar, se almacena la columna, y fila de su posicion
+-Se determina que espacio vacï¿½o cuenta con la mayor probabilidad de ganar, se almacena la columna, y fila de su posicion
 -Se realiza el movimiento por la maquina
 -Se imprime el tablero
 -Se repite hasta el fin del juego
@@ -36,18 +36,36 @@ Procedimiento general:
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 #define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
 
+void jugar(int);
+void simular();
 void imprimirTabla(char[][3]);
 void movimientoJugador(char[][3]);
-int movimientoIdeal(char[][3], int, int);
+int movimientoIdeal(char[][3], int, int, int*);
+int movimientoSimulado(char[][3], int, int);
 void colocarMovimiento(char[][3], int, int, char);
 int determinaPuntuacion(char[][3]);
 void turnoMaquina(char[][3]);
+void turnoSimulado(char[][3]);
 int esEmpate(char[][3]);
 
 int main(){
 
-    // Definimos el espacio de juego como una tabla de 3x3
-    char tabla[3][3] = {{'_', '_', '_'}, {'_', '_', '_'}, {'_', '_', '_'}};
+    int esSimulacion = 1; 
+    printf("\nIngresa 0 si deseas jugar, o 1 si deseas simular juegos\n");        
+    scanf("%d", &esSimulacion);
+    jugar(esSimulacion);    
+    
+}
+
+void jugar(int esSimulacion){
+    // Definimos el espacio de juego como una tabla de 3x3    
+    char tabla[3][3];
+    if(!esSimulacion){
+        char tabla[3][3] = {{'_', '_', '_'}, {'_', '_', '_'}, {'_', '_', '_'}};
+    }else {
+        char tabla[3][3] = {{'x', '_', '_'}, {'_', '_', 'x'}, {'o', '_', 'o'}};
+    }
+     
     printf("Bienvenido a gato master 1000 \n");
     imprimirTabla(tabla);
     //Verificamos que el juego no haya terminado
@@ -58,20 +76,21 @@ int main(){
         printf("\nEstoy pensando...\n");
         //De no haber terminado, la maquina realiza un movimiento
         if(!esEmpate(tabla) && (determinaPuntuacion(tabla) != 10) && (determinaPuntuacion(tabla) != -10)){
-            turnoMaquina(tabla);
+            esSimulacion ? turnoSimulado(tabla) : turnoMaquina(tabla);
             imprimirTabla(tabla);
         }
     }
 
     // Verificamos de forma termino el juego, victoria de maquina, jugador, o empate
     if(esEmpate(tabla)){
-        printf("\nHemos empatado!");
+        printf("\nHemos empatado!\n");
     }else if(determinaPuntuacion(tabla) == 10){
         printf("\nHe ganado! Perdedor");
     }else{
         printf("\nFelicidades, has ganado!");
     }
 }
+
 
 void imprimirTabla(char tabla[][3]){
     int columnas = 0;
@@ -96,7 +115,7 @@ void imprimirTabla(char tabla[][3]){
 void movimientoJugador(char tabla[][3]){
     int movimientoColumna = 0, movimientoFila = 0, esValido = 0;
 
-    // Determinamos si el movimiento del jugador es válido, esto para validar los datos ingresados
+    // Determinamos si el movimiento del jugador es vï¿½lido, esto para validar los datos ingresados
     while(!esValido){
         printf("\n Ingresa su posicion en columna ");
         scanf(" %i", &movimientoColumna);
@@ -121,32 +140,128 @@ void colocarMovimiento(char tabla[][3], int columna, int fila, char movimiento){
     tabla[columna][fila] = movimiento;
 }
 
-void turnoMaquina(char tabla[][3]){
+///////////////////////* Juego Simulado *///////////////////////////////
+
+void turnoSimulado(char tabla[][3]){
     // Determinamos una puntuacion, esta sera reemplazada por los movimientos probados por la maquina
-    int maximaPuntuacion = -100;
-    int filaIdeal;
-    int columnaIdeal;
+    int maximaPuntuacion = -100, filaIdeal, columnaIdeal, puntuacion = 0;
     for (int filas = 0; filas < 3; filas++){
         for (int columnas = 0; columnas < 3; columnas++){
             if(tabla[columnas][filas] == '_'){
                 // Buscamos los espacios vacios, los reemplazamos con un movimiento de la maquina
-                tabla[columnas][filas] = 'o';
+                tabla[columnas][filas] = 'o';                
                 // Comprobamos si la probabilidad de ganar de ese movimiento es mayor a la maxima probabilidad, de serlo, almacenamos la fila y columna
-                if(movimientoIdeal(tabla, 0, 0) > maximaPuntuacion){
-                    maximaPuntuacion = movimientoIdeal(tabla, 0, 0);
+                printf("\nSimulando movimientos a partir de columna %d fila %d\n", (columnas+1), (filas+1));
+                imprimirTabla(tabla);
+                puntuacion = movimientoSimulado(tabla, 0, 0);
+                if(puntuacion > maximaPuntuacion){                    
+                    maximaPuntuacion = puntuacion;
+                    filaIdeal = filas;
+                    columnaIdeal = columnas;
+                }                                
+                // Volvemos a vaciar el espacio, para probar otro movimiento
+                tabla[columnas][filas] = '_';
+            }
+        }
+    }
+    // Se coloca el movimiento en el espacio con la mayor probabilidad de ganar  
+    printf("\nEsta es la jugada ideal!\n");
+    colocarMovimiento(tabla, columnaIdeal, filaIdeal, 'o');
+}
+
+int movimientoSimulado(char tabla[][3], int esTurnoMaquina, int profundidad){
+    // Determinamos si el juego ha terminado, si gana la maquina, determinamos 10 puntos, si gana el jugador, -10
+    int puntuacion = determinaPuntuacion(tabla);
+
+    // De haber ganado la maquina, retornamos la puntuacion, menos la profunidad, esto para ganar en la menor cantidad de pasos posibles
+    if(puntuacion == 10){        
+        printf("En esta jugada gana la maquina!\n");
+        return (puntuacion-profundidad);
+    }
+
+    // De haber el jugador, retornamos la puntuacion mas la profundidad, esto por si la maquina ha de perder, hacerlo en la mayor cantidad de pasos posibles
+    if(puntuacion == -10){
+        printf("En esta jugada gana el jugador!\n");
+        return(puntuacion+profundidad);
+    }
+
+    //De ser empate retornamos 0
+    if(esEmpate(tabla)){
+        printf("En esta jugada termina en empate!\n");
+        return 0;
+    }
+
+    // Si el turno a simular es de la maquina
+    if(esTurnoMaquina){
+        // Definimos el mejor resultado posible como -100, ya que la maquina al ganar, debera retornar un resultado positivo
+        int mejor = -100;
+        for(int filas = 0; filas < 3; filas++){
+            for(int columnas = 0; columnas < 3; columnas++){
+                if(tabla[columnas][filas] == '_'){
+                    // Probamos un movimiento de la maquina
+                    tabla[columnas][filas] = 'o';
+                    printf("\nSimulando jugada de maquina de %d de profundidad\n", profundidad);
+                    // Determinamos la mayor probabilidad entre el movimiento encontrado, y el actual
+                    imprimirTabla(tabla);
+                    int puntuacionMovSimulado = movimientoSimulado(tabla, !esTurnoMaquina, profundidad+1);
+                    mejor = MAX(puntuacionMovSimulado, mejor);
+                    tabla[columnas][filas] = '_';
+                }
+            }
+        }
+        return mejor;
+    // Si el turno a simular es del jugador
+    }else {
+        // Definimos el mejor resultado posible como 100, ya que la maquina al perder, debera retornar un resultado negativo
+        int mejor = 100;
+        for(int filas = 0; filas < 3; filas++){
+            for(int columnas = 0; columnas < 3; columnas++){
+                if(tabla[columnas][filas] == '_'){
+                    // Simulamos un movimiento del jugador
+                    tabla[columnas][filas] = 'x';
+                    // Determinamos cual es la menor probabilidad, el movimiento actual, o el siguiente
+                    printf("\nSimulando jugada de jugador de %d de profundidad\n", profundidad);
+                    imprimirTabla(tabla);
+                    int puntuacionMovSimulado = movimientoSimulado(tabla, !esTurnoMaquina, profundidad+1);
+                    mejor = MIN(puntuacionMovSimulado, mejor);
+                    tabla[columnas][filas] = '_';
+                }
+            }
+        }
+        return mejor;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////7
+
+void turnoMaquina(char tabla[][3]){
+    // Determinamos una puntuacion, esta sera reemplazada por los movimientos probados por la maquina
+    int maximaPuntuacion = -100, simulaciones = 0, simulacionesTotales = 0, filaIdeal, columnaIdeal;
+    for (int filas = 0; filas < 3; filas++){
+        for (int columnas = 0; columnas < 3; columnas++){
+            if(tabla[columnas][filas] == '_'){
+                // Buscamos los espacios vacios, los reemplazamos con un movimiento de la maquina
+                tabla[columnas][filas] = 'o';                
+                // Comprobamos si la probabilidad de ganar de ese movimiento es mayor a la maxima probabilidad, de serlo, almacenamos la fila y columna
+                if(movimientoIdeal(tabla, 0, 0, &simulaciones) > maximaPuntuacion){
+                    simulaciones = 0;
+                    maximaPuntuacion = movimientoIdeal(tabla, 0, 0, &simulaciones);
                     filaIdeal = filas;
                     columnaIdeal = columnas;
                 }
+                simulacionesTotales++;
+                simulacionesTotales += simulaciones;
                 // Volvemos a vaciar el espacio, para probar otro movimiento
                 tabla[columnas][filas] = '_';
             }
         }
     }
     // Se coloca el movimiento en el espacio con la mayor probabilidad de ganar
+    printf("\nSe simularon %d jugadas\n", simulacionesTotales);
     colocarMovimiento(tabla, columnaIdeal, filaIdeal, 'o');
 }
 
-int movimientoIdeal(char tabla[][3], int esTurnoMaquina, int profundidad){
+int movimientoIdeal(char tabla[][3], int esTurnoMaquina, int profundidad, int* simulaciones){
     // Determinamos si el juego ha terminado, si gana la maquina, determinamos 10 puntos, si gana el jugador, -10
     int puntuacion = determinaPuntuacion(tabla);
 
@@ -174,8 +289,9 @@ int movimientoIdeal(char tabla[][3], int esTurnoMaquina, int profundidad){
                 if(tabla[columnas][filas] == '_'){
                     // Probamos un movimiento de la maquina
                     tabla[columnas][filas] = 'o';
+                    *simulaciones = *simulaciones + 1;
                     // Determinamos la mayor probabilidad entre el movimiento encontrado, y el actual
-                    mejor = MAX(movimientoIdeal(tabla, !esTurnoMaquina, profundidad+1), mejor);
+                    mejor = MAX(movimientoIdeal(tabla, !esTurnoMaquina, profundidad+1, simulaciones), mejor);
                     tabla[columnas][filas] = '_';
                 }
             }
@@ -190,8 +306,9 @@ int movimientoIdeal(char tabla[][3], int esTurnoMaquina, int profundidad){
                 if(tabla[columnas][filas] == '_'){
                     // Simulamos un movimiento del jugador
                     tabla[columnas][filas] = 'x';
+                    *simulaciones = *simulaciones + 1;
                     // Determinamos cual es la menor probabilidad, el movimiento actual, o el siguiente
-                    mejor = MIN(movimientoIdeal(tabla, !esTurnoMaquina, profundidad+1), mejor);
+                    mejor = MIN(movimientoIdeal(tabla, !esTurnoMaquina, profundidad+1, simulaciones), mejor);
                     tabla[columnas][filas] = '_';
                 }
             }
